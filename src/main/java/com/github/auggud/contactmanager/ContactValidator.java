ea0c8e220ec755a7b279f6218dc73605a311df3f
Augustas <augustasgudynas@gmail.com>
2025-11-24 15:42:55 +0200

    Object class created. Basic logic with objects created. Basic validation created.

--- src/main/java/com/github/auggud/contactmanager/ContactValidator.java
+++ src/main/java/com/github/auggud/contactmanager/ContactValidator.java
@@ -1,5 +1,18 @@
 package com.github.auggud.contactmanager;
 
 public class ContactValidator {
-    //validation (email format, phone rules, etc.)
+    // email validation
+    public static boolean isValidEmail(String email) {
+        return email != null && email.contains("@") && email.contains(".com");
+    }
+
+    // phone validation
+    public static boolean isValidPhone(String phone) {
+        return phone != null && phone.matches("\\+?[0-9]+");
+    }
+
+    // name validation
+    public static boolean isValidName(String name) {
+        return  name != null && !name.isBlank();
+    }
 }
